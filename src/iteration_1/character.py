37910668d03e25bbbfd1c3a651efeeba988a5a77
jenna-theresa-01 <jennamar828@gmail.com>
2023-10-25 20:04:50 +0000

    iteration 1, feature 6 test passed

--- src/iteration_1/character.py
+++ src/iteration_1/character.py
@@ -1,15 +1,18 @@
 class Char:
-    # armor_class = 10
-    # hit_points = 5
-    # name = ""
+    armor_class = 10
+    hit_points = 5
+    name = ""
+    
 
-
-
-
-
+    
     def __init__(self, name, align):
         self.name = name
         self.align = align
-
         
-
+    def declare_attack(self, defender, roll):
+        if roll >= defender.armor_class:
+            defender.hit_points -= 1
+        elif roll == 20:
+            defender.hit_points -= 2
+        
+            
