6b37e3fdd7f6cf1f6c629121dfe62849e386473f
jenna-theresa-01 <jennamar828@gmail.com>
2023-10-25 20:24:19 +0000

    updates

--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -2,22 +2,22 @@ import pytest
 from iteration_1.character import *
 
 def test_char_name():
-    character = Char("potato", "Evil")
+    character = Char("Justin", "Evil")
     assert character.name is not None
 
 
 def test_char_align():
-    character = Char("potato", "Evil")
+    character = Char("Justin", "Evil")
     assert character.align is not None
 
 
 def test_armor_class():
-    character = Char("potato", "Evil")
+    character = Char("Justin", "Evil")
     assert character.armor_class == 10
 
 
 def test_hit_points():
-    character = Char("potato", "Evil")
+    character = Char("Justin", "Evil")
     assert character.hit_points == 5
 
 
@@ -31,3 +31,7 @@ def test_get_hurt():
     tanner = Char("Tanner", "Good")
     justin.declare_attack(tanner, 18)
     assert tanner.hit_points == 4
+
+
+def test_ability_score():
+    
