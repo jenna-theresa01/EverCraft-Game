1c0ea000d4c31e0445ac7a845adf30ae14276456
jenna-theresa-01 <jennamar828@gmail.com>
2023-10-25 20:23:35 +0000

    updates before home

--- src/iteration_1/character.py
+++ src/iteration_1/character.py
@@ -2,8 +2,13 @@ class Char:
     armor_class = 10
     hit_points = 5
     name = ""
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
     
-
     
     def __init__(self, name, align):
         self.name = name
@@ -15,4 +20,27 @@ class Char:
         elif roll == 20:
             defender.hit_points -= 2
         
-            
+
+class Skill_Mods:
+    {
+    1 : -5,
+    2 : -4, 
+    3 : -4,
+    4 : -3,
+    5 : -3,
+    6 : -2,
+    7 : -2,
+    8 : -1,
+    9 : -1,
+    10 : 0,
+    11 : 0,
+    12 : 1,
+    13 : 1,
+    14 : 2,
+    15 : 2,
+    16 : 3,
+    17 : 3,
+    18 : 4,
+    19 : 4,
+    20 : 5,
+    }
