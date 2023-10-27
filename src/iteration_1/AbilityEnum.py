1e968f7b3376c1deec0637dd0f0388f70f2c1fa9
jenna-theresa-01 <jennamar828@gmail.com>
2023-10-27 11:24:36 +0000

    iteration 1 done

--- src/iteration_1/AbilityEnum.py
+++ src/iteration_1/AbilityEnum.py
@@ -1,53 +1,25 @@
-from enum import Enum
+# from enum import Enum
 
-class Abilities(Enum):
-    STRENGTH = 1
-    DEXTERITY = 2
-    CONSTITUTION = 3
-    WISDOM = 4
-    INTELLIGENCE = 5
-    CHARISMA = 6
-
-    def __init__(self, abilities):
-        self.abilities = abilities
-        self._score = 10
-        self._mod = 0
-
-    def get_score(self):
-        return self._score
-
-    def get_mod(self):
-        return self._mod
-
-    def set_score(self, v):
-        self._score = v
-        self._mod = self.switcher(v)
-    
-    score = property(get_score, set_score)
-
-    mod = property(get_mod)
-
-    def mod_values(self, score):
+class Modifiers():
         mods = {
-        1: -5,
-        2: -4,
-        3: -4,
-        4: -3,
-        5: -3,
-        6: -2,
-        7: -2,
-        8: -1,
-        9: -1,
-        10: 0,
-        11: 0,
-        12: 1,
-        13: 1,
-        14: 2,
-        15: 2,
-        16: 3,
-        17: 3,
-        18: 4,
-        19: 4,
-        20: 5
+        "Value_1": -5,
+        "Value_2": -4,
+        "Value_3": -4,
+        "Value_4": -3,
+        "Value_5": -3,
+        "Value_6": -2,
+        "Value_7": -2,
+        "Value_8": -1,
+        "Value_9": -1,
+        "Value_10": 0,
+        "Value_11": 0,
+        "Value_12": 1,
+        "Value_13": 1,
+        "Value_14": 2,
+        "Value_15": 2,
+        "Value_16": 3,
+        "Value_17": 3,
+        "Value_18": 4,
+        "Value_19": 4,
+        "Value_20": 5
         }
-        return mods.get(score)
--- src/iteration_1/character.py
+++ src/iteration_1/character.py
@@ -2,25 +2,54 @@ from iteration_1.AbilityEnum import *
 from iteration_1.AbilityScores import *
 
 
-class Char:
+class Char():
     armor_class = 10
     hit_points = 5
     name = ""
     xp = 0
     level = 1
-    abilities = []
-
+    abilities = {
+        "Strength" : 10,
+        "Dexterity" : 10,
+        "Constitution" : 15,
+        "Wisdom" : 10,
+        "Intelligence" : 10,
+        "Charisma" : 10,
+    }
+    roll = int()
     attack_status = ""    
     
+    # def set_ability_scores(self):
+    #     for ability in Abilities:
+    #         ability_value = AbilityScore[ability]
+    #         self.abilities.append(ability_value)
+
     def __init__(self, name, align):
         self.name = name
         self.align = align
-        self.set_ability_scores()
             
-    def set_ability_scores(self):
-        for i in Abilities:
-            ability_value = Abilities[i]
-            self.abilities.append(ability_value)
+    # def set_ability_scores(self):
+    #     for i in Abilities:
+    #         ability_value = Abilities[i]
+    #         self.abilities.append(ability_value)
+
+    def apply_strength_mod(self, roll):
+        strength_value = self.abilities["Strength"]
+        mod_value = Modifiers.mods[f"Value_{strength_value}"]
+        self.roll += mod_value
+        return self.roll
+
+    def apply_dexterity_mod(self):
+        dexterity_value = self.abilities["Dexterity"]
+        mod_value = Modifiers.mods[f"Value_{dexterity_value}"]
+        self.armor_class += mod_value
+        return self.armor_class
+
+    def apply_constitution_mod(self):
+        constitution_value = self.abilities["Constitution"]
+        mod_value = Modifiers.mods[f"Value_{constitution_value}"]
+        self.hit_points += mod_value
+        return self.hit_points
 
     def declare_attack(self, defender, roll):
         if roll >= defender.armor_class:
